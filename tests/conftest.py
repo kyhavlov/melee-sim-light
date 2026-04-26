07720d48c15920bb3ce27cfb5d7de977ea4523b4
Kyle Havlovitz <kylehav@gmail.com>
2026-04-26 03:16:12 -0700

    Accept current shield tilt test artifacts

--- tests/conftest.py
+++ tests/conftest.py
@@ -369,7 +369,7 @@ def _ensure_shield_tilt_bins() -> None:
                 with out.open("rb") as f:
                     magic = f.read(8)
                     ver = int.from_bytes(f.read(4), "little", signed=False)
-                if magic == b"MSLSHLD1" and ver == 1:
+                if magic == b"MSLSHLD1" and ver in (1, 2, 3):
                     continue
             except OSError:
                 pass
